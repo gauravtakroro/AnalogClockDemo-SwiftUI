2aa1fc7803dede2263a7e2d585d47bf02184d3f4
Gaurav Tak <gaurav.tak@roro.io>
2023-12-25 02:00:35 +0530

    settings icon and ui appearance icon added

--- AnalogClockDemo-SwiftUI/HomeView.swift
+++ AnalogClockDemo-SwiftUI/HomeView.swift
@@ -10,8 +10,31 @@ import SwiftUI
 struct HomeView: View {
     
     var body: some View {
-        Image(systemName: "clock").padding(.bottom, 5)
-        Text("Analog Clock").foregroundColor(Color.primary)
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                  print("moon icon tapped")
+                }, label: {
+                    Image(systemName: "moon.fill")
+                        .font(.system(size: 16))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.primary)
+                        .clipShape(Circle())
+                })
+                Button(action: {
+                    print("settings icon tapped")
+                }, label: {
+                    Image(systemName: "gearshape")
+                        .font(.system(size: 36))
+                        .foregroundColor(.black)
+                        .padding()
+                        .clipShape(Circle())
+                })
+            }.padding(.trailing, 20)
+            Spacer()
+        }
     }
 }
 
