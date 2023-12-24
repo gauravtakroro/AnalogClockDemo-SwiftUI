ada208d5d02a86dfd46415ac9ca45f21a55b1aae
Gaurav Tak <gaurav.tak@roro.io>
2023-12-24 12:10:50 +0530

    Initial Commit

---
+++ AnalogClockDemo-SwiftUI/AnalogClockDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockDemo_SwiftUIApp.swift
+//  AnalogClockDemo-SwiftUI
+//
+//  Created by Roro Solutions LLP on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
